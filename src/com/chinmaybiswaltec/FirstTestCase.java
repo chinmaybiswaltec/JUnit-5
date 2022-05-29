e23557f372a2a876ee24414ead2afe12255eb28b
chinmaybiswaltech <chinmaybiswaltech@gmail.com>
2022-05-29 10:41:07 +0530

    first commit

---
+++ src/com/chinmaybiswaltec/FirstTestCase.java
@@ -0,0 +1,11 @@
+package com.chinmaybiswaltec;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstTestCase {
+	
+	@Test
+	public void firstTest() {
+		System.out.println("First Test");
+	}
+}
